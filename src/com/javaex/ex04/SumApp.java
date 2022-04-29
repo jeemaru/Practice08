ea1e12a69dea79f977a9ddb294693bd17d314690
jeemaru <4iiii@hanmail.net>
2022-04-29 18:50:43 +0900

    Practice08

---
+++ src/com/javaex/ex04/SumApp.java
@@ -0,0 +1,24 @@
+package com.javaex.ex04;
+
+import java.util.Scanner;
+
+public class SumApp {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 공백으로 구분해서 입력하세요");
+		int numLine = sc.nextInt();
+
+		//코드를 작성하세요
+		
+		sum = numLine+sum;
+		
+		
+		System.out.println("합:" + sum);
+		
+		sc.close();
+	}
+
+}
